803f112e678c6e492e586036166a53a6f4acfeee
Nadeem22 <nadeem2107@gmail.com>
2020-04-07 21:08:46 +0530

    initial commit

---
+++ src/main/java/com/nadeem/spring/security/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.nadeem.spring.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String getWelcomeMessage(){
+        return "Welcome";
+    }
+}
---
+++ src/test/java/com/nadeem/spring/security/SecurityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nadeem.spring.security;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
